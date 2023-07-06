a1196b7699387c0e2f836959ce79b525bdd122dd
Ahmed-Elhalwagy <ahmed.elhalwagy88@gmail.com>
2023-07-06 06:07:35 +0300

    Added the sort feature

--- src/App.js
+++ src/App.js
@@ -89,11 +89,33 @@ function Form({ handelAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onDoubleClickItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  switch (sortBy) {
+    case "input":
+      sortedItems = items;
+      break;
+    case "description":
+      sortedItems = [...items].sort((a, b) =>
+        a.description.localeCompare(b.description)
+      );
+      break;
+    case "packed":
+      sortedItems = items
+        .slice()
+        .sort((a, b) => Number(a.packed) - Number(b.packed));
+      break;
+    default:
+      sortedItems = items;
+  }
+
   return (
     <>
       <div className="list">
         <ul>
-          {items.map((item) => {
+          {sortedItems.map((item) => {
             return (
               <Item
                 item={item}
@@ -104,6 +126,13 @@ function PackingList({ items, onDeleteItem, onDoubleClickItem }) {
             );
           })}
         </ul>
+        <div className="actions">
+          <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+            <option value="input">sort By input order</option>
+            <option value="description">sort By description</option>
+            <option value="packed">sort By status</option>
+          </select>
+        </div>
       </div>
     </>
   );
